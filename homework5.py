793c357acb2d8afafa11f025aa9c7718f14b3e59
BoldyrevaMarya <matazzz@mail.ru>
2024-06-30 22:13:30 +0300

    Add files via upload

---
+++ homework5.py
@@ -0,0 +1,10 @@
+immutable_var=(15.2+4.5),[4,5,6],"xxx"
+print(type(immutable_var))
+print(immutable_var)
+#immutable_var[2]="ooo"
+
+mutable_list=["cat","dog","bird"]
+mutable_list[0]="lion"
+mutable_list[1]="snake"
+mutable_list[2]="dragon"
+print(mutable_list)
